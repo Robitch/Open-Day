9bcb5f559dc19da601de87560eaaf2e77f774a12
robpoi-vm <robin.poiron@tangram.fr>
2024-10-16 10:53:44 +0200

    feat: add new /subtract endpoint

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/addition', (req, res) => {
-  const {a, b} = req.query;
+  const { a, b } = req.query;
   const sum = parseFloat(a) + parseFloat(b);
   res.send(`The sum is ${sum}`);
+});
+
+app.get('/subtraction', (req, res) => {
+  const { a, b } = req.query;
+  const difference = parseFloat(a) - parseFloat(b);
+  res.send(`The difference is ${difference}`);
 });
 
 app.listen(port, () => {
